45811dc164b99b33f0bee4ec4a1e100a9a239227
Eugene Otieno <otienoeo@gmail.com>
2022-12-31 08:13:05 +0300

    Ingredient calculator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,6 +8,19 @@ Pouring some milk into the cup
 Coffee is ready!
 """
 
+const val ONE_CUP_WATER = 200
+const val ONE_CUP_MILK = 50
+const val ONE_CUP_BEANS = 15
+
 fun main() {
-    println(STEPS)
+    println("Write how many cups of coffee you will need: ")
+    val cups = readln().toInt()
+    val water = cups * ONE_CUP_WATER
+    val milk = cups * ONE_CUP_MILK
+    val beans = cups * ONE_CUP_BEANS
+
+    println("For $cups cups of coffee you will need: ")
+    println("$water ml of water")
+    println("$milk ml of milk")
+    println("$beans g of coffee beans")
 }
